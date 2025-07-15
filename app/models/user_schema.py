d8a63aea8da2a9aad5983b9c3795bc872fad179a
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-07-15 18:39:45 +0545

    db models created

---
+++ app/models/user_schema.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class UserTable(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index = True)
+    username = Column(String, unique=True)
+    email = Column(String, unique= True)
+    full_name = Column(String)
+    hashed_password = Column(String)
+    is_disabled = Column(Boolean, default=False)
