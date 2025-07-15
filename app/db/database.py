86dd6381d5ce1bbe13fa316ecbaf0c3d9b3a16f9
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-07-15 18:41:23 +0545

    db config made

---
+++ app/db/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+print("Database URL>", SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+
