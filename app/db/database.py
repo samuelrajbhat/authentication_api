35f2325033727fc83d2fc3abf696397cd865149f
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-07-21 17:47:30 +0545

    use sqlalchemy session

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
-
+from models.user_models import Base
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL>", SQLALCHEMY_DATABASE_URL)
@@ -9,6 +9,16 @@ print("Database URL>", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    
+    # except Exception as e:
+    #     print (f"Database connection error {e}")
+    finally: 
+        db.close()
 
 
 
