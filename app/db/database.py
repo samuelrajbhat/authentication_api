7babf4c70237027849c8aa511ad666f9805e27f0
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-07-30 18:46:19 +0545

    authenticated user verification route added

--- app/db/database.py
+++ app/db/database.py
@@ -2,26 +2,15 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import declarative_base
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL>", SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
-# c = engine.connect()
-# try:
-#     c.execute(text("SELECT * FROM users"))
-#     c.close()
-# except exc.DBAPIError as e:
-#     if  e.connection_invalidated: 
-#         print ('Cnnection was invalidated!')
-
-# c = engine.connect()
-# c.execute(text("SELECT * FROM users"))
-
 SessionLocal = sessionmaker(bind=engine)
-
+Base = declarative_base()
 def get_db():
     db = SessionLocal()
     try:
