08c2f0012ed05ad7cad818088fe9e91ee1fab0e3
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-08-01 12:32:19 +0545

    .env path change adjusted

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from pathlib import Path
-env__path = Path(".")/".env"
+env__path = Path(__file__).resolve().parents[2]/".env"
 load_dotenv(dotenv_path=env__path)
 
 class Settings:
