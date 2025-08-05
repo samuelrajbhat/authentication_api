ff76a2a856c52726eecce8393b7545853fc107d8
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-08-05 18:38:11 +0545

    Implement context schema

--- app/langgraph_app/tool_wrapper.py
+++ app/langgraph_app/tool_wrapper.py
@@ -1,11 +1,14 @@
 from langchain_core.tools import tool
+from langgraph.runtime import get_runtime
+# from langgraph_app.agent_graph import Context
 
-from .crud_function import add_todo, get_todos
-
+from .crud_function import add_todo, get_todos, update_todo_status, delete_todo_item
 @tool
-def fetch_todos(token: str) -> str:
+def fetch_todos() -> str:
     """fetch all todos form the  user"""
-    todos = get_todos(unclean_token=token)
+    runtime = get_runtime(Context)
+    # token_ = runtime.context.token
+    todos = get_todos( runtime.context.token) # type: ignore
     return str(todos)
 
 @tool
@@ -14,4 +17,16 @@ def create_todos(token:str, title: str, description: str, status: str):
     new_todos = add_todo(unclean_token=token, title=title, description=description, status=status)
     return new_todos
 
-tools = [fetch_todos, create_todos]
+@tool
+def update_todo(token: str, new_status: str, todo_id: int):
+    """Update the status of a todo with given todo_id."""
+    updated_todo = update_todo_status(token=token, new_status=new_status, todo_id = todo_id)
+    return  updated_todo
+
+@tool
+def delete_todo(token: str, todo_id: int):
+    """Delete a todo item with the given todo_id."""
+    deleted_todo = delete_todo_item(token= token, todo_id= todo_id)
+    return deleted_todo
+
+tools = [fetch_todos, create_todos, update_todo, delete_todo]
