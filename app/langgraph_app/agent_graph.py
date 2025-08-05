2badc173904111cecb0e47edce2b6a9dbd7319fa
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-08-05 18:36:39 +0545

    Add context schema

--- app/langgraph_app/agent_graph.py
+++ app/langgraph_app/agent_graph.py
@@ -1,9 +1,10 @@
 from langgraph.graph import StateGraph, END, add_messages
 from langchain_openai import ChatOpenAI
 from typing import TypedDict, Annotated, Literal
-from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import ToolNode, create_react_agent
 from .tool_wrapper import tools
 from langchain_core.messages import HumanMessage, SystemMessage
+from langgraph.runtime import Runtime
 
 llm = ChatOpenAI(model="gpt-4")
  
@@ -11,12 +12,20 @@ llm = ChatOpenAI(model="gpt-4")
 class State(TypedDict):
     messages: Annotated[list, add_messages]
     token: str
-def agent(state: State):
-   messages = state["messages"]
-   token = state["token"]
-   llm_with_tools = llm.bind_tools(tools)
-   response = llm_with_tools.invoke(messages)
-   return {'messages': [response]}
+
+class Context(TypedDict):
+    token: str
+
+# agent_model = create_react_agent( model="openai:gpt-4", tools=[tools],context_schema=Context)
+
+def agent(state: State, runtime: Runtime[Context]):
+    """Agent that processes messages and invokes tools if needed."""
+    messages = state["messages"]
+    token = state["token"]
+    llm_with_tools = llm.bind_tools(tools)
+    response = llm_with_tools.invoke(messages)
+    # response = agent_model.invoke(messages, context={"token": token})
+    return {'messages': [response]}
 
 tool_node = ToolNode(tools)
 
@@ -26,7 +35,7 @@ def should_continue(state)-> Literal["tools", END]:  # type: ignore
     if last_message.tool_calls:
         return "tools"
     return END
-graph_builder = StateGraph(State)
+graph_builder = StateGraph(state_schema=State, context_schema= Context)
 
 graph_builder.add_node("agent", agent)
 graph_builder.add_node("tools", tool_node)
@@ -39,10 +48,10 @@ graph = graph_builder.compile()
 
 def run_graph(user_input: str, token: str):
     result = graph.invoke({
-        "messages": [SystemMessage(content=f"The user's tokenn is {token}. Use this when calling tools."),
+        "messages": [
             HumanMessage(content=user_input)],
         "token": token
-    })
+    }, context={"token": token})
     last_ai_message = result["messages"][-1].content
     return last_ai_message
 
