6d773d090ffa17d8ff93a6a31ae53aa7b3df4b6b
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-08-06 15:56:09 +0545

    Added checkpointer

--- app/langgraph_app/agent_graph.py
+++ app/langgraph_app/agent_graph.py
@@ -6,22 +6,18 @@ from .tool_wrapper import tools
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.runtime import Runtime
 from .agent_models import Context, State
+from langgraph.checkpoint.memory import InMemorySaver
+
+
 llm = ChatOpenAI(model="gpt-4")
+
+checkpointer = InMemorySaver()
  
 
-# class State(TypedDict):
-#     messages: Annotated[list, add_messages]
-#     token: str
 
-# class Context(TypedDict):
-#     token: str
-
-# agent_model = create_react_agent( model="openai:gpt-4", tools=[tools],context_schema=Context)
-
-def agent(state: State, runtime: Runtime[Context]):
+def agent(state: State):
     """Agent that processes messages and invokes tools if needed."""
     messages = state["messages"]
-    token = state["token"]
     llm_with_tools = llm.bind_tools(tools)
     response = llm_with_tools.invoke(messages)
     # response = agent_model.invoke(messages, context={"token": token})
@@ -44,14 +40,15 @@ graph_builder.set_entry_point("agent")
 graph_builder.add_conditional_edges("agent", should_continue)
 graph_builder.add_edge("tools", "agent")
 
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=checkpointer)
 
 def run_graph(user_input: str, token: str):
     result = graph.invoke({
         "messages": [
             HumanMessage(content=user_input)],
-        "token": token
-    }, context={"token": token})
+        "token": token},
+        {"configurable": {"thread_id": "1"}}, # type: ignore
+        context={"token": token})
     last_ai_message = result["messages"][-1].content
     return last_ai_message
 
